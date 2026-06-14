eb2bf167ddeee6346772dc97150afcef9e97bf50
neal.nguyen <nnchau26@gmail.com>
2026-06-14 22:32:18 +0700

    feat: add atribute status in UpdateJobDto

--- src/business/jobs/dto/job.dto.ts
+++ src/business/jobs/dto/job.dto.ts
@@ -1,6 +1,6 @@
 ﻿import { IsEnum, IsNotEmpty, IsOptional, IsString, IsDateString, IsInt, IsIn } from 'class-validator';
 import { Type } from 'class-transformer';
-import { JobType, ShipmentMode } from '../../../models/job.entity';
+import { JobStatus, JobType, ShipmentMode } from '../../../models/job.entity';
 import { PaginationDto } from '../../../common/dto/pagination.dto';
 
 export class CreateJobDto {
@@ -41,6 +41,7 @@ export class CreateJobDto {
 export class UpdateJobDto {
   @IsOptional() @IsEnum(JobType) jobType?: JobType;
   @IsOptional() @IsEnum(ShipmentMode) shipmentMode?: ShipmentMode;
+  @IsOptional() @IsEnum(JobStatus) status?: JobStatus;
   @IsOptional() @IsInt() partnerId?: number;
   @IsOptional() @IsInt() branchId?: number;
   @IsOptional() @IsInt() assignedUserId?: number;
